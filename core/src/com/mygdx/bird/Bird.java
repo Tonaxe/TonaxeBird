0b9848c13f4a3802b8ed84e61f0ab96286f53fd6
Tonaxe <ymennana@elpuig.xeill.net>
2024-03-18 16:56:11 +0100

    Initial commit

---
+++ core/src/com/mygdx/bird/Bird.java
@@ -0,0 +1,59 @@
+package com.mygdx.bird;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class Bird extends Game {
+	SpriteBatch batch;
+	Texture img;
+	BitmapFont smallFont, bigFont;
+
+	AssetManager manager;
+
+
+	@Override
+	public void create() {
+		batch = new SpriteBatch();
+		// Create bitmap fonts from TrueType font
+		FreeTypeFontGenerator generator = new
+				FreeTypeFontGenerator(Gdx.files.internal("8bitOperatorPlus-Bold.ttf"));
+		FreeTypeFontGenerator.FreeTypeFontParameter params = new
+				FreeTypeFontGenerator.FreeTypeFontParameter();
+		params.size = 22;
+		params.borderWidth = 2;
+		params.borderColor = Color.BLACK;
+		params.color = Color.WHITE;
+		smallFont = generator.generateFont(params); // font size 22 pixels
+		params.size = 50;
+		params.borderWidth = 5;
+		bigFont = generator.generateFont(params); // font size 50 pixels
+		generator.dispose(); // don't forget to dispose to avoid memory leaks!
+
+		manager = new AssetManager();
+		manager.load("bird.png", Texture.class);
+		manager.load("pipe_up.png", Texture.class);
+		manager.load("pipe_down.png", Texture.class);
+		manager.load("background.png", Texture.class);
+		manager.finishLoading();
+
+		this.setScreen(new MainMenuScreen(this));
+	}
+
+	@Override
+	public void render() {
+		super.render(); // important!
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+}
