3aeac044d7930de390ab85081f102a627eda7972
Tonaxe <ymennana@elpuig.xeill.net>
2024-03-20 23:29:13 +0100

    sisu

--- core/src/com/mygdx/bird/GameScreen.java
+++ core/src/com/mygdx/bird/GameScreen.java
@@ -1,14 +1,21 @@
 package com.mygdx.bird;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class GameScreen implements Screen {
     final Bird game;
 
     OrthographicCamera camera;
+
+    Stage stage;
+    Player player;
+    boolean dead;
+
 
     public GameScreen(Bird game) {
         this.game = game;
@@ -17,15 +24,42 @@ public class GameScreen implements Screen {
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
 
+        player = new Player();
+        player.setManager(game.manager);
+        stage = new Stage();
+        stage.getViewport().setCamera(camera);
+        stage.addActor(player);
+
+        dead = false;
+
+
     }
 
     @Override
     public void render(float delta) {
 
-        // clear the screen with a color
-        ScreenUtils.clear(0.3f, 0.8f, 0.8f, 1);
+        //LOGICA ========================================================
+        stage.act();
         // tell the camera to update its matrices.
         camera.update();
+        // process user input
+        if (Gdx.input.justTouched()) {
+            player.impulso();
+        }
+
+        // Comprova que el jugador no es surt de la pantalla.
+        // Si surt per la part inferior, game over
+        if (player.getBounds().y > 480 - player.getHeight())
+            player.setY( 480 - player.getHeight() );
+        if (player.getBounds().y < 0 - player.getHeight()) {
+            dead = true;
+        }
+
+
+
+        //RENDER ========================================================
+        // clear the screen with a color
+        ScreenUtils.clear(0.3f, 0.8f, 0.8f, 1);
         // tell the SpriteBatch to render in the
         // coordinate system specified by the camera.
         game.batch.setProjectionMatrix(camera.combined);
@@ -33,6 +67,9 @@ public class GameScreen implements Screen {
         game.batch.begin();
         game.batch.draw(game.manager.get("background.png", Texture.class),0, 0);
         game.batch.end();
+
+        stage.getBatch().setProjectionMatrix(camera.combined);
+        stage.draw();
 
     }
 
